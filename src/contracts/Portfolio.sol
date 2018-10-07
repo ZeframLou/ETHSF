854a5063438bbc8605d607bca9d4a9645fe8914d
Zefram Lou <zeframlou@gmail.com>
2018-10-06 19:49:36 -0700

    now reusable

--- src/contracts/Portfolio.sol
+++ src/contracts/Portfolio.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.4.24;
 
 import "./dharma/DebtKernel.sol";
 import "./dharma/RepaymentRouter.sol";
+import "./dharma/TermsContract.sol";
 import "./kyber/KyberNetworkProxyInterface.sol";
 import "zeppelin/ownership/Ownable.sol";
 import "zeppelin/math/SafeMath.sol";
@@ -11,27 +12,29 @@ contract Portfolio is Ownable {
     using SafeMath for uint256;
 
     address public constant KYBER = 0x0;
-    address public constant TERMS_CONTRACT = 0x0;
     address public constant DEBT_KERNEL = 0x0;
     address public constant REPAYMENT_ROUTER = 0x0;
     uint256 public constant PRECISION = 10 ** 18;
     uint256 public constant MAX_AMOUNT = 10 ** 28;
 
+    address public termsContractAddress;
     address public borrowedTokenAddress;
     address[] public assetList;
     uint256[] public fractionList;
     uint256[] public assetAmountList;
+    bytes32 public agreementId;
 
     KyberNetworkProxyInterface public kyber;
     DebtKernal public debtKernel;
     RepaymentRouter public repaymentRouter;
+    TermsContract public termsContract;
 
     ERC20 public borrowedToken;
 
     constructor (address[] _assetList, uint256[] _fractionList) public {
         assetList = _assetList;
         fractionList = _fractionList;
-        
+
         kyber = KyberNetworkProxyInterface(KYBER);
         debtKernel = DebtKernel(DEBT_KERNEL);
         repaymentRouter = RepaymentRouter(REPAYMENT_ROUTER);
@@ -46,18 +49,27 @@ contract Portfolio is Ownable {
         bytes32[3] signaturesR,
         bytes32[3] signaturesS
     ) public onlyOwner {
+        if (termsContractAddress != address(0)) {
+            // check if the previous loan has ended
+            require(termsContract.getExpectedRepaymentValue(agreementId, termsContract.getTermEndTimestamp(agreementId)) == 0);
+        }
+
+        // initialize terms related contracts
+        termsContractAddress = orderAddresses[3];
+        termsContract = TermsContract(termsContractAddress);
         borrowedTokenAddress = orderAddresses[4];
         borrowedToken = ERC20(borrowedTokenAddress);
-        uint256 beforeTokenBalance = borrowedToken.balanceOf(this);
 
         // fill debt order and get funds
-        bytes32 agreementId = debtKernel.fillDebtOrder(creditor, orderAddresses, orderValues, orderBytes32, signaturesV, signaturesR, signaturesS);
+        uint256 beforeTokenBalance = borrowedToken.balanceOf(this);
+        agreementId = debtKernel.fillDebtOrder(creditor, orderAddresses, orderValues, orderBytes32, signaturesV, signaturesR, signaturesS);
 
         // calculate received loan
         uint256 receivedLoan = borrowedToken.balanceOf(this).sub(beforeTokenBalance);
 
         // buy tokens using Kyber
         bytes memory hint;
+        delete assetAmountList;
         for (uint256 i = 0; i < assetList.length; i++) {
             uint256 slippage;
             uint256 srcAmount = receivedLoan.mul(fractionList[i]).div(PRECISION);
