92dac229e5616195fb47b9840546e51da44b745c
Zefram Lou <zeframlou@gmail.com>
2018-10-06 19:11:18 -0700

    removed set

--- src/contracts/Portfolio.sol
+++ src/contracts/Portfolio.sol
@@ -5,9 +5,6 @@ import "./dharma/RepaymentRouter.sol";
 import "./kyber/KyberNetworkProxyInterface.sol";
 import "zeppelin/ownership/Ownable.sol";
 import "zeppelin/math/SafeMath.sol";
-import "./set/ISetToken.sol";
-import "./set/ICore.sol";
-import "./set/ISetFactory.sol";
 import "./kyber/ERC20Interface.sol";
 
 contract Portfolio is Ownable {
@@ -17,12 +14,9 @@ contract Portfolio is Ownable {
     address public constant TERMS_CONTRACT = 0x0;
     address public constant DEBT_KERNEL = 0x0;
     address public constant REPAYMENT_ROUTER = 0x0;
-    address public constant SET_CORE = 0x0;
-    address public constant SET_FACTORY = 0x0;
     uint256 public constant PRECISION = 10 ** 18;
     uint256 public constant MAX_AMOUNT = 10 ** 28;
 
-    address public setTokenAddress;
     address public borrowedTokenAddress;
     address[] public assetList;
     uint256[] public fractionList;
@@ -30,10 +24,7 @@ contract Portfolio is Ownable {
     KyberNetworkProxyInterface public kyber;
     DebtKernal public debtKernel;
     RepaymentRouter public repaymentRouter;
-    ICore public setCore;
-    ISetFactory public setFactory;
 
-    ISetToken public setToken;
     ERC20 public borrowedToken;
 
     constructor (address _borrowedToken) public {
@@ -43,7 +34,6 @@ contract Portfolio is Ownable {
         kyber = KyberNetworkProxyInterface(KYBER);
         debtKernel = DebtKernel(DEBT_KERNEL);
         repaymentRouter = RepaymentRouter(REPAYMENT_ROUTER);
-        setCore = ICore(SET_CORE);
     }
 
     function startPortfolio(
@@ -73,8 +63,6 @@ contract Portfolio is Ownable {
             destAmountList.push(tradeWithHint(borrowedToken, srcAmount, components[i], this, MAX_AMOUNT,
                 slippage, 0, hint));
         }
-
-        // create SetToken
         
     }
 }
